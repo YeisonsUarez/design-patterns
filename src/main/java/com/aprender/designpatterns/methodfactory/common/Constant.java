9403fe583fb3d6e509622a13ed11e85c91ec3e63
Lisseth <yhernandez557@unab.edu.co>
2024-08-19 12:42:43 -0500

    Create Constant.java

---
+++ src/main/java/com/aprender/designpatterns/methodfactory/common/Constant.java
@@ -0,0 +1,11 @@
+package com.aprender.designpatterns.methodfactory.common;
+
+public interface Constant {
+
+    String EMAIL_NOTIFICATION = "EMAIL";
+
+    String PUSH_NOTIFICATION = "PUSH";
+
+    String SMS_NOTIFICATION = "SMS";
+
+}
